43a81ef6fa8f53f1d91d8b7ce27909e139820255
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-12-28 07:51:08 +0100

    feat: add nodejs integration tests for verifier

--- packages/verifier/vite.config.ts
+++ packages/verifier/vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    exclude: ["**/*.browser.test.ts", "**/node_modules/**"],
+    exclude: ["**/*.browser.test.ts", "**/*.browser.integration.test.ts", "**/node_modules/**"],
   },
 });
